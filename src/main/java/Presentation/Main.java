1f6de4c5aedc8f55a9b178d3b525f76be165be0d
YAHYA TAIMOURYA <TAIMOURYA_YAHYA@emsi-edu.ma>
2021-03-22 16:00:27 +0100

    injection fichier txt

--- src/main/java/Presentation/Main.java
+++ src/main/java/Presentation/Main.java
@@ -1,16 +1,44 @@
 package Presentation;
 
 import Dao.DaoImp;
+import Dao.Idao;
+import Metier.IMetier;
 import Metier.MetierImp;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
+    public static IMetier metier;
+    public static Idao dao;
+
+    public static void initDependance()  {
+
+        try {
+            FileInputStream file = new FileInputStream("C:\\Users\\yahya\\IdeaProjects\\jeeAt1\\src\\main\\java\\dependance.txt");
+            Scanner sc = new Scanner(file);
+            Class daoClass = Class.forName(sc.nextLine());
+            dao = (Idao)daoClass.newInstance();
+
+            Class metierClass = Class.forName(sc.nextLine());
+            metier = (IMetier)metierClass.newInstance();
+            ((MetierImp)metier).setDao(dao);
+
+        } catch (ClassNotFoundException | FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
     public static void main(String[] args) {
+        initDependance();
 
-        DaoImp dao = new DaoImp();
-        MetierImp metierImp = new MetierImp();
-
-        metierImp.setDao(dao);
-        
-        System.out.println("calculer : " + metierImp.calculer());
+        System.out.println("calculer : " + metier.calculer());
     }
 }
